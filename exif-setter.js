4a8cd9480fdeae213421a1bb632a48b219e888e1
joshsj89 <joshsj89@gmail.com>
2023-06-20 22:29:05 -0700

    Now works for Samsung camera jpegs

--- exif-setter.js
+++ exif-setter.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const piexif = require('piexifjs');
 
 /*
@@ -11,8 +12,21 @@ const piexif = require('piexifjs');
 //'2023:01:01 12:00:00'
 //'2023:05:26 04:51:41'
 
-const imagePath = 'JPEG-Filename-to-EXIF/images/20230526_045141.jpg';
-const modifiedImagePath = 'JPEG-Filename-to-EXIF/images/modified.jpg';
+const folderPath = 'JPEG-Filename-Date-Stamper/images/';
+
+fs.readdir(folderPath, (err, files) => {
+    if (err) {
+        console.error('Error reading folder:', err);
+        return;
+    }
+
+    const jpgFiles = files.filter(file => path.extname(file).toLowerCase() === '.jpg');
+
+    jpgFiles.forEach(file => {
+        const filePath = path.join(folderPath, file);
+        updateImageExifData(filePath);
+    });
+});
 
 const getBinaryDataFromJpegFile = (filename) => {
     return fs.readFileSync(filename).toString('binary');
@@ -27,16 +41,36 @@ const getJpegFileFromBinaryData = (base64String, filename) => {
     fs.writeFileSync(filename, imageBuffer);
 }
 
-const im = getExifFromJpegFile(imagePath);
+const getNewTimestamp = (filepath) => {
+    const filename = filepath.substring(filepath.lastIndexOf("\\") + 1);
 
-im.Exif['36867'] = '2023:01:01 12:00:00';
-im.Exif['36868'] = '2023:01:01 12:00:00';
+    const year = filename.substring(0, 4);
+    const month = filename.substring(4, 6);
+    const day = filename.substring(6, 8);
+    const hour = filename.substring(9, 11);
+    const minute = filename.substring(11, 13);
+    const second = filename.substring(13, 15);
+    const newTimestamp = `${year}:${month}:${day} ${hour}:${minute}:${second}`;
+    return newTimestamp;
+}
 
-const imDump = piexif.dump(im)
-
-const temp = piexif.insert(imDump, getBinaryDataFromJpegFile(imagePath));
-console.log(typeof temp);
-
-getJpegFileFromBinaryData(temp, modifiedImagePath);
+const updateImageExifData = (imagePath) => {
+    const exifData = getExifFromJpegFile(imagePath);
+    
+    const newTimestamp = getNewTimestamp(imagePath);
+    
+    exifData.Exif['36867'] = newTimestamp;
+    exifData.Exif['36868'] = newTimestamp;
+    
+    const exifDump = piexif.dump(exifData)
+    
+    const temp = piexif.insert(exifDump, getBinaryDataFromJpegFile(imagePath));
+    
+    getJpegFileFromBinaryData(temp, imagePath);
+}
 
 console.log('Complete');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Next Steps:
+//      -Expand to include other naming formats
