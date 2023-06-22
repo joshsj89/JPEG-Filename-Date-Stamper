d1ca1e06954df0453a8e758931e738df3baa30cf
joshsj89 <joshsj89@gmail.com>
2023-06-21 19:49:44 -0700

    Now works for 2 different formats
    YYYYMMDD_HHMMSS (ISO 8601)
    MMDDYYHHMM

--- exif-setter.js
+++ exif-setter.js
@@ -29,15 +29,38 @@ const getJpegFileFromBinaryData = (base64String, filename) => {
 
 const getNewTimestamp = (filepath) => {
     const filename = filepath.substring(filepath.lastIndexOf("\\") + 1);
-    
-    const year = filename.substring(0, 4);
-    const month = filename.substring(4, 6);
-    const day = filename.substring(6, 8);
-    const hour = filename.substring(9, 11);
-    const minute = filename.substring(11, 13);
-    const second = filename.substring(13, 15);
-    const newTimestamp = `${year}:${month}:${day} ${hour}:${minute}:${second}`;
-    return newTimestamp;
+
+    if (ss = filename.match(/\d{8}_\d{6}/g)) { // ISO 8601 (YYYYMMDD_HHMMSS)
+        const year = ss[0].substring(0, 4);
+        const month = ss[0].substring(4, 6);
+        const day = ss[0].substring(6, 8);
+        const hour = ss[0].substring(9, 11);
+        const minute = ss[0].substring(11, 13);
+        const second = ss[0].substring(13, 15);
+
+        const newTimestamp = `${year}:${month}:${day} ${hour}:${minute}:${second}`;
+        return newTimestamp;
+
+    } else if (ss = filename.match(/\d{10}/g)) { // (MMDDYYHHMM)
+        const month = ss[0].substring(0, 2);
+        const day = ss[0].substring(2, 4);
+        let year = ss[0].substring(4, 6);
+        const hour = ss[0].substring(6, 8);
+        const minute = ss[0].substring(8, 10);
+
+        if (year > 50) {
+            year = `19${year}`;
+        } else {
+            year = `20${year}`;
+        }
+
+        const newTimestamp = `${year}:${month}:${day} ${hour}:${minute}:00`;
+        return newTimestamp;
+
+    } else {
+        console.error('Filename does not match any expected format:', filename);
+        return null;
+    }
 }
 
 const updateImageExifData = (imagePath) => {
